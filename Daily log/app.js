048eee2a1244afad95979e6de365fc6f38467b2e
Miyu Kobayashi <miyu.koabayshi329@gamil.com>
2025-05-06 13:39:10 -0400

    2025-5-5-Javascript2

---
+++ Daily log/app.js
@@ -0,0 +1,20 @@
+
+let random = Math.random();
+if (random < 0.5) {
+    console.log("0.5より小さい");
+    console.log(random);
+}
+
+if (random >= 0.5) {
+    console.log("数字は0.5以上")
+    console.log(random);
+}
+
+/*Practice
+
+function isEven(num) {
+if (num % 2 === 0){
+console.log("even");
+}
+}
+*/
