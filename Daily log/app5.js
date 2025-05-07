a7833d7313672e54a16e3328c41a6508b6a0c8a2
Miyu Kobayashi <miyu.koabayshi329@gamil.com>
2025-05-07 15:22:07 -0400

    2025-5-7-Javascript

---
+++ Daily log/app5.js
@@ -0,0 +1,132 @@
+let colors = []
+
+undefined
+colors
+[]length: 0[[Prototype]]: Array(0)
+let days = ["Monday", "Tuesday", "Wednesday"];
+undefined
+days
+    (3)['Monday', 'Tuesday', 'Wednesday']
+days.length
+3
+let hoge = [true, undefiend, 12 9.999, Nan, null, "hji"];
+VM413: 1 Uncaught SyntaxError: Unexpected numberUnderstand this error
+length + 7
+7
+days[1][0]
+'T'
+let firstName = "ken";
+undefined
+fistName[0]
+VM567: 1 Uncaught ReferenceError: fistName is not defined
+at < anonymous >: 1: 1
+    (anonymous) @VM567: 1Understand this error
+let colors = ["rad", "orange", "yallow"];
+undefined
+colors[0] = "red"
+'red'
+colors
+    (3)['red', 'orange', 'yallow']
+colors[2] = "yellow";
+'yellow'
+
+let beatles = ["paul", "jhon", "george"];
+undefined
+beatles
+    (3)['paul', 'jhon', 'george']
+beatles[3] = "ringo";
+'ringo'
+beatles
+    (4)['paul', 'jhon', 'george', 'ringo']
+
+
+//     let movieLina =["yamada", "yato", "suzuki"];
+// undefined
+// movieLine.push("sato");
+// VM1227:1 Uncaught ReferenceError: movieLine is not defined
+//     at <anonymous>:1:1
+// (anonymous) @ VM1227:1Understand this error
+// movieLine
+// VM1259:1 Uncaught ReferenceError: movieLine is not defined
+//     at <anonymous>:1:1
+// (anonymous) @ VM1259:1Understand this error
+// movieLina.push(sato);
+// VM1329:1 Uncaught ReferenceError: sato is not defined
+//     at <anonymous>:1:16
+// (anonymous) @ VM1329:1Understand this error
+// movieLina
+// (3) ['yamada', 'yato', 'suzuki']
+// movieLina.push("sato");
+
+// 4
+// movieLina
+// (4) ['yamada', 'yato', 'suzuki', 'sato']
+// movieLina.push("Fuji", "Yama", "Kato");
+
+
+// 7
+// movieLina.pop();
+// 'Kato'
+// movieLina
+// (6) ['yamada', 'yato', 'suzuki', 'sato', 'Fuji', 'Yama']
+// let person = movieLina.pop();
+// undefined
+// person
+// 'Yama'
+// movieLina.shift();
+// 'yamada'
+// movieLina
+// (4) ['yato', 'suzuki', 'sato', 'Fuji']
+// let nexPerson = movieLina.shift();
+// undefined
+// nextPerson
+// VM1805:1 Uncaught ReferenceError: nextPerson is not defined
+//     at <anonymous>:1:1
+// (anonymous) @ VM1805:1Understand this error
+// nexPerson
+// 'yato'
+// movieLina..push(kimura);
+// VM1923:1 Uncaught SyntaxError: Unexpected token '.'Understand this error
+// movieLina.push(kimura);
+
+// VM1927:1 Uncaught ReferenceError: kimura is not defined
+//     at <anonymous>:1:16
+// (anonymous) @ VM1927:1Understand this error
+// movieLina.push(#"kimura");
+
+// VM1938:1 Uncaught SyntaxError: Invalid or unexpected tokenUnderstand this error
+// movieLina.push("kimura");
+
+
+// 4
+// movieLine.shift();
+// VM1996:1 Uncaught ReferenceError: movieLine is not defined
+//     at <anonymous>:1:1
+// (anonymous) @ VM1996:1Understand this error
+// movieLina.shift();
+
+// 'suzuki'
+// movieLina.shift();
+
+
+// 'sato'
+// movieLina.shift();
+
+
+// 'Fuji'
+// movieLina.shift();
+
+
+// 'kimura'
+// movieLina.shift();
+
+
+// undefined
+// movieLina
+// []
+// movieLina.push("Tan", "wan","jhon", "Dan");
+// 4
+// movieLina.unshift("vip");
+// 5
+// movieLina
+// (5) ['vip', 'Tan', 'wan', 'jhon', 'Dan']
