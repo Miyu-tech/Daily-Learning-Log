1ea0b166262db12367dff3c391ecf0657e4b79f7
Miyu Kobayashi <miyu.koabayshi329@gamil.com>
2025-06-09 14:38:44 -0400

    app20.js

---
+++ Daily log/app20.js
@@ -0,0 +1,15 @@
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const buttons = document.querySelectorAll('button');
+
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        button.style.backgroundColor = makeRandomColor();
+    });
+}
