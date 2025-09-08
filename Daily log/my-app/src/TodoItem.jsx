f5dde1f8797a439a6eecea2aa34d111da2bdbf36
Miyu Kobayashi <miyu.kobayashi329@gmail.com>
2025-09-07 21:57:07 -0400

    2025-9-8

---
+++ Daily log/my-app/src/TodoItem.jsx
@@ -0,0 +1,43 @@
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import CommentIcon from '@mui/icons-material/Comment';
+
+export default function TodoList({ todo, removeTodo, toggle }) {
+    const labelId = `checkbox-list-label-${todo.id}`;
+
+    return (
+        <ListItem
+            secondaryAction={
+                <IconButton
+                    edge="end"
+                    aria-label="comments"
+                    onClick={() => removeTodo(todo.id)}>
+                    <CommentIcon />
+                </IconButton>
+            }
+            disablePadding
+        >
+            <ListItemButton
+                role={undefined}
+                onClick={toggle}
+                dense>
+                <ListItemIcon>
+                    <Checkbox
+                        edge="start"
+                        checked={todo.completed}
+                        tabIndex={-1}
+                        disableRipple
+                        inputProps={{ 'aria-labelledby': labelId }}
+                    // onChange={toggle}
+                    />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={todo.text} />
+            </ListItemButton>
+        </ListItem>
+    );
+}
