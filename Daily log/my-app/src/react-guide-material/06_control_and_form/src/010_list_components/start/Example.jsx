496b78534b243b3d88bf5fd8d4da0eb3e59cba77
Miyu Kobayashi <miyu.kobayashi329@gmail.com>
2025-09-30 14:47:21 -0400

    2025-9-30

---
+++ Daily log/my-app/src/react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
@@ -0,0 +1,28 @@
+
+const animals = ["Dog", "Cat", "Rat"];
+
+const Example = () => {
+  const animalList = [];
+for (const animal of animals) {
+  animalList.push(<li>{animal}</li>);
+}
+
+const helloAnimals animals.map((animal) => 
+<li>Hello, {animall}</li> );
+return (
+    <>
+      <h3>配列の操作</h3>
+      <ul>
+        {/* <li>{animals[0]}</li>
+        <li>{animals[1]}</li>
+        <li>{animals[2]}</li> */}
+
+      </ul>
+    </>
+  );
+};
+
+export default Example;
+
+
+// map is frequetly use in React more than for()
