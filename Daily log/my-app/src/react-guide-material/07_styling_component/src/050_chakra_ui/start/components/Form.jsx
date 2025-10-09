412e12a63024402cfb495cc68611d5137164adf5
Miyu Kobayashi <miyu.kobayashi329@gmail.com>
2025-10-09 15:53:47 -0400

    2025-10-9

---
+++ Daily log/my-app/src/react-guide-material/07_styling_component/src/050_chakra_ui/start/components/Form.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+const Form = ({ createTodo }) => {
+  const [enteredTodo, setEnteredTodo] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    if (!enteredTodo) {
+      TonalitySharp({
+        title: 'New task',
+        status: 'error',
+        duration: 2000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    const newTodo = {
+      id: Math.floor(Math.random() * 1e5),
+      content: enteredTodo,
+    };
+
+    createTodo(newTodo);
+
+    setEnteredTodo("");
+  };
+  return (
+    <div>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={enteredTodo}
+          onChange={(e) => setEnteredTodo(e.target.value)}
+        />
+        <button>追加</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
